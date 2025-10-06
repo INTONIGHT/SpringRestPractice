a9b4372edb06ba66efb6181fd676d9010b0f8423
INTONIGHT <tyler.throop@gmail.com>
2025-10-06 06:09:09 -0700

    starting to build out a very simple post controller

--- src/main/java/com/spring/practice/testing/controller/ProductController.java
+++ src/main/java/com/spring/practice/testing/controller/ProductController.java
@@ -1,13 +1,18 @@
 package com.spring.practice.testing.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.practice.testing.model.Product;
 
 @RestController
 public class ProductController {
-	
-	@GetMapping("/")
-	public String hello() {
-		return "Hello World";
+	//request body annotation : automatically binds parameters into the parameters
+	@PostMapping("/product")
+	public String createProduct(@RequestBody final Product product) {
+		System.out.println(product);
+		return "12345";
 	}
 }
---
+++ src/main/java/com/spring/practice/testing/controller/model/ProductID.java
@@ -0,0 +1,8 @@
+package com.spring.practice.testing.controller.model;
+
+public class ProductID {
+	private final String id;
+	public ProductID(final String id) { this.id = id;}
+	
+	public String getId() { return this.id; }
+}
